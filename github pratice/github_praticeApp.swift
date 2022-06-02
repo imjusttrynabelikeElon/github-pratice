48ac0ce50b0ca4fc41044398d2dadc0b950ba157
karon bell <karonbell5218@gmail.com>
2022-06-02 00:39:03 -0400

    Initial Commit

---
+++ github pratice/github_praticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_praticeApp.swift
+//  github pratice
+//
+//  Created by Karon Bell on 6/2/22.
+//
+
+import SwiftUI
+
+@main
+struct github_praticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
